85495c56d1b997e74e89712bf133835c9bc193f1
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-13 10:06:46 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  arr.each.with_index {|name, index| puts "#{index + 1}: #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr
   # Your code here
+  arr.map {|arr| "#{arr.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls arr
   # Your code here
+  arr.each { |arr| 
+    if arr.size > 4
+      return true
+  end
+    }
+    return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each { |cheese|
+    if arr.include? cheese
+      return cheese
+    end
+    }
+    return nil
 end
